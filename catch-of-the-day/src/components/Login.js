63f5dd91ad10a960e6419eb54761b06ee92f18e5
Antony Finnegan <afinnegan@gmail.com>
2022-02-22 12:06:31 +0000

    Course updates

---
+++ catch-of-the-day/src/components/Login.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Login = (props) => (
+    <nav className="login">
+        <h2>Inventory Login</h2>
+        <p>Sign in to manage your stores inventory</p>
+        <button className="github" onClick={() => props.authenticate('Github')}>
+            Login In With GitHub
+        </button>
+        <button className="facebook" onClick={() => props.authenticate('Facebook')}>
+            Login In With Facebook
+        </button>
+    </nav>
+);
+
+Login.propTypes = {
+    authenticate: PropTypes.func.isRequired
+            
+};
+
+export default Login;
